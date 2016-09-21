3affcb336dce3215b87b83c7449ec69919d88565
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-09-21 15:58:51 -0700

    valid types for multiply

--- src/types/ProgrammingWithNothingAndTypes.kt
+++ src/types/ProgrammingWithNothingAndTypes.kt
@@ -8,12 +8,12 @@ import org.junit.Test
 typealias L<T> = (T) -> T
 typealias LL<T> = L<L<T>>
 typealias LLL<T> = L<L<L<T>>>
-typealias N<T> = N2<T, T>
+typealias N<T> = ((T) -> T) -> (T) -> T
 typealias NN<T> = N<N<T>>
-typealias N2<T,U> = ((T) -> U) -> (T) -> U
+//typealias N2<T,U> = ((T) -> U) -> (T) -> U
 
-fun <T,U> zero(): N2<T,U> = { p -> { x -> x as U }}
-fun <T,U> one(): N2<T,U> = { p -> { x -> p(x) }}
+fun <T> zero(): N<T> = { p -> { x -> x }}
+fun <T> one(): N<T> = { p -> { x -> p(x) }}
 fun <T> two(): N<T> = { p -> { x -> p(p(x)) }}
 fun <T> three(): N<T> = { p -> { x -> p(p(p(x))) }}
 fun <T> five(): N<T> = { p -> { x -> p(p(p(p(p(x))))) }}
@@ -30,13 +30,16 @@ fun B<Boolean>.toKotlin(): Boolean = this(true)(false)
 
 
 fun <T> increment(): (N<T>) -> N<T> = { n -> { p -> { x -> p(n(p)(x)) }}}
-//fun <T> decrement(): (N<T>) -> N<T> =
-//        { n: N<T> -> { f: L<T> -> { x ->
-//            n({ g:LL<T> -> { h: LLL<T> -> h(g(f)) }})({ y:T -> x })({ y: T -> y })
+//fun <T> decrement(): (N<L<T>>) -> N<T> =
+//        { n -> { f: L<T> -> { x: T ->
+//            val xl = { y:T -> x }
+//            val yl = { y:T -> y }
+//            val function/*: L<T> */= { g: LL<T> -> { h: LLL<T> -> h(g(f)) } }
+//            n(function)(xl)(yl)
 //        }}}
-fun <T> add(): (N<T>) -> (NN<T>) -> N<T> = { m -> { n -> n(increment())(m) }}
+fun <T> add(): (NN<T>) -> (N<T>) -> N<T> = { n -> { m -> n(increment<T>())(m) }}
 //fun <T> subtract(): (N<T>) -> (N<T>) -> N<T> = { m -> { n -> n(decrement<T>())(m) }}
-fun <T> multiply(): (N<T>) -> (N2<NN<T>, N<T>>) -> N<T> = { m -> { n ->
+fun <T> multiply(): (NN<T>) -> (NN<T>) -> N<T> = { n -> { m ->
     n(add<T>()(m))(zero())
 }}
 
--- test/types/ProgrammingWithNothingAndTypesTest.kt
+++ test/types/ProgrammingWithNothingAndTypesTest.kt
@@ -9,14 +9,13 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.Test
 import toInt
-import types.N2
 import types.toKotlin
 import types.zero
 
 class ProgrammingWithNothingAndTypesTest {
     @Test fun `conversion to kotlin ints`() {
-        assertThat(zero<Int,Int>().toKotlin(), equalTo(0))
-        assertThat(one<Int,Int>().toKotlin(), equalTo(1))
+        assertThat(zero<Int>().toKotlin(), equalTo(0))
+        assertThat(one<Int>().toKotlin(), equalTo(1))
         assertThat(two<Int>().toKotlin(), equalTo(2))
     }
 
@@ -56,7 +55,7 @@ class ProgrammingWithNothingAndTypesTest {
         assertThat(multiply<Int>()(zero())(zero()).toInt(), equalTo(0))
         assertThat(multiply<Int>()(zero())(one()).toInt(), equalTo(0))
         assertThat(multiply<Int>()(one())(one()).toInt(), equalTo(1))
-//        assertThat(multiply<Int>()(three())(five()).toInt(), equalTo(15))
+        assertThat(multiply<Int>()(three())(five()).toInt(), equalTo(15))
     }
 
 }
